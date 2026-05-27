c8c15cec59160727f1617f717455447d958d674d
José Pereira da Silva Neto <devneto203@gmail.com>
2026-05-27 19:37:49 -0300

    feat: Implement QueenState class with constructors and methods for state management

--- src/model/QueenState.java
+++ src/model/QueenState.java
@@ -1,5 +1,32 @@
 package model;
 
+import java.util.Arrays;
+
 public class QueenState {
 
+    private int[] queens;
+    private int currentRow;
+
+    public QueenState() {
+        this.queens = new int[8];
+        Arrays.fill(this.queens, -1);
+        this.currentRow = 0;
+    }
+
+    public QueenState(int[] queens, int currentRow) {
+        this.queens = queens;
+        this.currentRow = currentRow;
+    }
+
+    public int[] getQueens() {
+        return queens;
+    }
+
+    public int getCurrentRow() {
+        return currentRow;
+    }
+
+    public boolean isGoal() {
+        return currentRow == 8;
+    }
 }
