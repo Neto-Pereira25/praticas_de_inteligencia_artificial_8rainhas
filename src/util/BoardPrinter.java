1d871839281fb05f20a061860c7b196f467c88e3
José Pereira da Silva Neto <devneto203@gmail.com>
2026-05-27 19:41:02 -0300

    feat: Add printBoard method for visualizing the Eight Queens board state

--- src/util/BoardPrinter.java
+++ src/util/BoardPrinter.java
@@ -2,4 +2,20 @@ package util;
 
 public class BoardPrinter {
 
+    public static void printBoard(int[] queens) {
+
+        for (int row = 0; row < 8; row++) {
+
+            for (int col = 0; col < 8; col++) {
+
+                if (queens[row] == col) {
+                    System.out.print(" Q ");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
 }
